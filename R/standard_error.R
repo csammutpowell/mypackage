ccab26603c8b977bf9220142ed2f9ba636a49638
Camilla Sammut-Powell <cjmspowell@gmail.com>
2020-01-07 13:30:24 +0000

    updated documentation

--- R/standard_error.R
+++ R/standard_error.R
@@ -1,11 +1,12 @@
-#' Calculate the standard error for a sample
-#'returns the of a numeric vector
+#' Calculate standard error of a sample
+#'
 #' @param v numeric vector
 #'
 #' @return number
 #' @export
 #'
-#' @examples standard_error(rnorm(50))
+#' @examples
+#' standard_error(rnorm(50))
 standard_error <- function(v){
-  var(v)/sqrt(length(v))
+  sqrt(sum_squares(v)/(length(v)))
 }
--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,12 +1,12 @@
-#' Calculate the sums of squares for a sample
-#'returns the sum of the squared deviations
-#' from mean, SS(x) of a numeric vector
+#' Calculate sum of squares for a sample
+#'
 #' @param v numeric vector
 #'
 #' @return number
 #' @export
 #'
-#' @examples sum_squares(rnorm(50))
+#' @examples
+#' sum_squares(rnorm(50))
 sum_squares <- function(v){
   sum((v - mean(v))^2)
 }
